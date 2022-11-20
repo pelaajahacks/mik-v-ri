ece1b7e994f4009382084b4a281380264d41aee7
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2022-11-20 23:11:35 +0200

    made so when you click the color spinner you see more details

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ function getColor() {
         url: "https://api.segso.net/hsl/color?jsonp=true",
         type: 'GET',
         dataType: 'jsonp',
-        crossDomain: true ,
-        contentType:'application/json',
+        crossDomain: true,
+        contentType: 'application/json',
         secure: true,
         headers: {
             "accept": "application/json",
             'Access-Control-Allow-Origin': '*',
         },
-        success: function (data){
+        success: function(data) {
             $("#colorhex1").text(data.color[0].hexColor);
             $("#colorrgb1").text(data.color[0].rgbColor);
             $("#colorhex2").text(data.color[1].hexColor);
             $("#colorrgb2").text(data.color[1].rgbColor);
-            $("#example1").css({"background-color": data.color[0].hexColor});
-            $("#example2").css({"background-color": data.color[1].hexColor});
-            
+            $("#example1").css({ "background-color": data.color[0].hexColor });
+            $("#example2").css({ "background-color": data.color[1].hexColor });
+            $("#example1").attr("href", "./colors/" + data.color[0].hexColor.slice(1, 7));
+            $("#example2").attr("href", "./colors/" + data.color[1].hexColor.slice(1, 7));
+
         }
-        });
-    
+    });
+
 };
 
 function getTicketColor() {
@@ -28,18 +30,16 @@ function getTicketColor() {
         url: "https://api.segso.net/hsl/color?jsonp=true",
         type: 'GET',
         dataType: 'jsonp',
-        crossDomain: true ,
-        contentType:'application/json',
+        crossDomain: true,
+        contentType: 'application/json',
         secure: true,
         headers: {
             "accept": "application/json",
             'Access-Control-Allow-Origin': '*',
         },
-        success: function (data){
-            $(".svgspin").css({"background-color": data.color[0].hexColor});
-            
+        success: function(data) {
+            $(".svgspin").css({ "background-color": data.color[0].hexColor });
+
         }
-        });
+    });
 };
-
-
